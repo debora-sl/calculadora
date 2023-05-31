1fc33c8fb9f49a1e9145b67b52caf7bec58688f9
debora-sl <debora.souzalima22@gmail.com>
2023-05-30 22:51:45 -0300

    Entregando projeto na data combinada.

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 //pegando o visor da calculadora
 const visor = document.querySelector('[data-visor]');
-console.log("Resultado: " + visor.textContent);
+console.log("Visor: " + visor.textContent);
 
-//pegando o botão de número
-const botoesNumericos = document.querySelector('[data-botoes-numeros]');
-console.log(botoesNumericos);
+//pegando a div que contém os botões
+let botoes = document.querySelector(".js-botoes");
 
+//adicionando um evento para verificar qual botão númerico foi clicado
+botoes.addEventListener('click', (e)=>{
 
-
-botoesNumericos.addEventListener('click', ()=>{
 });
-    const botaoNumero = document.querySelector('[data-botao-numero]');
-    botaoNumero.addEventListener('click', ()=>{
-        console.log('fui clicado');
-    });
+
+
+
+
+
+
+
+
+
 
 
 
